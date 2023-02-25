9bb5c12459640cfccd3075ffb989d151c4afcd54
Vladlen <vladlen@vozhzhaev.ru>
2023-02-25 20:32:23 +0300

    js

--- addArticle.php
+++ addArticle.php
@@ -1,11 +1,12 @@
 <div class="container my-3">
-    <div class="col-sm-6 mx-auto">
+    <div class="col-sm-9 mx-auto">
         <form action="/addArticle" method="post">
             <div class="mb-3">
                 <input type="text" class="form-control" name="title" placeholder="Заголовок">
             </div>
             <div class="mb-3">
-                <textarea name="content" placeholder="Контент" class="form-control"></textarea>
+                <!--<textarea name="content" placeholder="Контент" class="form-control"></textarea>-->
+                <textarea id="sample">Hi</textarea>
             </div>
             <div class="mb-3">
                 <input name="author" type="text" class="form-control" placeholder="Автор">
@@ -16,3 +17,16 @@
         </form>
     </div>
 </div>
+<link href="https://cdn.jsdelivr.net/npm/suneditor@latest/dist/css/suneditor.min.css" rel="stylesheet">
+<!-- <link href="https://cdn.jsdelivr.net/npm/suneditor@latest/assets/css/suneditor.css" rel="stylesheet"> -->
+<!-- <link href="https://cdn.jsdelivr.net/npm/suneditor@latest/assets/css/suneditor-contents.css" rel="stylesheet"> -->
+<script src="https://cdn.jsdelivr.net/npm/suneditor@latest/dist/suneditor.min.js"></script>
+<!-- languages (Basic Language: English/en) -->
+<script src="https://cdn.jsdelivr.net/npm/suneditor@latest/src/lang/ru.js"></script>
+<script>
+    const editor = SUNEDITOR.create((document.getElementById('sample') || 'sample'),{
+        lang: SUNEDITOR_LANG['ru'],
+        width: '100%',
+        height: '400px'
+    });
+</script>
